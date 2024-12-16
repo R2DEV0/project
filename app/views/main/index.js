8471a4acae9d99ccac5bf0e1ce845ee4ff34355b
Kevin Chancey <r2devo@gmail.com>
2024-12-15 19:18:14 -0800

    ui updates

--- app/views/main/index.js
+++ app/views/main/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState, useRef } from "react";
+import Facebook from '../../../public/facebook.svg';
 
 const Main = () => {
     const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
@@ -54,15 +55,16 @@ const Main = () => {
         <div
             id="background"
             style={{
-                background: `radial-gradient(circle at ${mousePosition.x}px ${mousePosition.y}px, rgba(197, 197, 197, 0.2), transparent 50%)`,
+                background: `radial-gradient(circle at ${mousePosition.x}px ${mousePosition.y}px, rgba(103, 94, 224, 0.15), transparent 30%)`,
             }}
             className="h-screen overflow-hidden"
         >
             <div className="flex h-full">
-                <div className="hidden md:flex flex-col justify-start items-start text-white w-2/4 p-20">
+                <div className="hidden md:flex flex-col justify-start items-start text-white w-2/4 p-28">
                     <h1 className="text-5xl title">Kevin Chancey</h1>
                     <p className="text-xl mt-2 subtitle">Full-Stack Developer</p>
-                    <p className="text-lg mt-5 phrase lg:w-1/2 md:w-3/4">Crafting elegant solutions for complex problems, from front to back.</p>
+                    <p className="mt-5 phrase lg:w-1/2 md:w-3/4">I craft elegant solutions for complex problems, from front to back.</p>
+
                     <nav className="mt-8 space-y-4">
                         <button
                             onClick={() => handleNavigation("section1")}
@@ -89,15 +91,23 @@ const Main = () => {
                             PROJECTS
                         </button>
                     </nav>
+
+                    <div className="flex items-center gap-5 justify-center mt-20">
+                        <img src="/linkedin.svg" alt="LinkedIn" width={23} title="LinkedIn" />
+                        <img src="/github.svg" alt="Instagram" width={25} title="Github" />
+                        <img src="/insta.svg" alt="Instagram" width={25} title="Instagram" />
+                        <img src="/facebook.svg" alt="Facebook" width={23} title="Facebook" />
+                    </div>
                 </div>
 
                 {/* Right Side: Scrollable Content */}
                 <div ref={rightColumnRef} className="flex-1 overflow-y-auto text-white md:px-10 px-10">
                     <div className="md:hidden pt-8">
-                        <h1 className="text-2xl font-bold">Kevin Chancey</h1>
-                        <p className="mt-2">Crafting elegant solutions for complex problems, from front to back.</p>
+                        <h1 className="text-3xl font-bold">Kevin Chancey</h1>
+                        <p className="text-lg mt-2 subtitle">Full-Stack Developer</p>
+                        <p className="mt-4 text-sm phrase">Crafting elegant solutions for complex problems, from front to back.</p>
                     </div>
-                    <section id="section1" className="h-screen pt-20">
+                    <section id="section1" className="h-screen md:pt-28 pt-20">
                         <h2 className="text-2xl font-bold mb-4">Section 1</h2>
                         <p>Content for section 1...</p>
                     </section>
